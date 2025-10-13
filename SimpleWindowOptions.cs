e173575204bdee51cbd3b364089916e82bcc50e0
dom <domportera@gmail.com>
2025-10-13 12:27:44 -0400

    add window optiion for rendering speed

--- SimpleWindowOptions.cs
+++ SimpleWindowOptions.cs
@@ -7,6 +7,7 @@ namespace ImGuiWindows
         int Fps,
         bool Vsync,
         WindowSizeFlags SizeFlags,
+        bool RenderLoopWaitsForEvents,
         bool AlwaysOnTop = false)
     {
         public bool IsResizable =>
